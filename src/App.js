bcf94750cbe362eb25499ca0e1c20f7e82d7e8df
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-18 11:15:10 -0400

    Update App component to use programatic nav.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import { useState, useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import NavBar from "./components/NavBar"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate()
 
+  const login = () => {
+    setIsLoggedIn(true)
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false)
+  }
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/")
+    } else {
+      navigate("/login")
+    }
+  }, [isLoggedIn])
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      <NavBar logout={logout} />
+      <Outlet context={login} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
